e205f25fe9f3c4d32450c1b643bc7fcd7c7d7dcf
Davit Soitashvili <davit.soitashvili.1@btu.edu.ge>
2021-06-26 14:14:55 +0400

    add dashboard bottom navigation

---
+++ app/src/main/java/com/example/giftsapp/ui/fragments/dashboard/create_post/CreatePostFragment.kt
@@ -0,0 +1,7 @@
+package com.example.giftsapp.ui.fragments.dashboard.create_post
+
+import androidx.fragment.app.Fragment
+import com.example.giftsapp.R
+
+class CreatePostFragment : Fragment(R.layout.fragment_create_post) {
+}
