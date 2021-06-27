2d6662000792f95c215ec3092e5735cec948cbb0
Davit Soitashvili <davit.soitashvili.1@btu.edu.ge>
2021-06-27 18:26:07 +0400

    add create post input validations, update

--- app/src/main/java/com/example/giftsapp/ui/fragments/dashboard/create_post/CreatePostFragment.kt
+++ app/src/main/java/com/example/giftsapp/ui/fragments/dashboard/create_post/CreatePostFragment.kt
@@ -11,6 +11,10 @@ import com.example.giftsapp.tools.extensions.clear
 import com.example.giftsapp.tools.extensions.removeDots
 import com.example.giftsapp.tools.extensions.removeMailSign
 import com.example.giftsapp.tools.showToastMessage
+import com.example.giftsapp.tools.validators.InputValidationException
+import com.example.giftsapp.tools.validators.InputValidators.validateOnEmptyInput
+import com.example.giftsapp.tools.validators.InputValidators.validateOnPostDescriptionLength
+import com.example.giftsapp.tools.validators.InputValidators.validateOnPostTitleLength
 import com.google.firebase.database.FirebaseDatabase
 import java.util.*
 
@@ -32,6 +36,15 @@ class CreatePostFragment : Fragment(R.layout.fragment_create_post) {
     private fun createPost() {
         with(binding) {
             createPostBtn.setOnClickListener {
+                try {
+                    validateOnEmptyInput(inputPostTitleView)
+                    validateOnEmptyInput(inputPostDescriptionView)
+                    validateOnPostTitleLength(inputPostTitleView)
+                    validateOnPostDescriptionLength(inputPostDescriptionView)
+                } catch (exception: InputValidationException) {
+                    return@setOnClickListener
+                }
+
                 val title = inputPostTitleView.text.toString().trim()
                 val description = inputPostDescriptionView.text.toString().trim()
                 val author = App.app!!.getFirebaseAuth().currentUser?.email
